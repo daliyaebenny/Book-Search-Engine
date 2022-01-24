92e2b7a4a0ac635c5418b5a78aaeea590d3d0b8d
Daliya Benny <daliyaebenny@gmail.com>
2022-01-24 03:17:13 -0800

    Added queris and mutation

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -32,6 +32,16 @@ const typeDefs = gql`
     title: String!
   }
 
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
